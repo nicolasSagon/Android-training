d935bac4a7328fbbeb420a0e5d37d217f6212214
nicolasSagon <sagon.nicolas@gmail.com>
2023-08-10 15:26:08 +0200

    Codelab persist data with room

---
+++ basic-android-kotlin-compose-training-inventory-app-persist-with-room/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(version = 1, exportSchema = false, entities = [Item::class])
+abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also {
+                        Instance = it
+                    }
+            }
+        }
+    }
+}
