cfbdbbadc0fddc097e1f23fc909659fc6631a971
nicolasSagon <sagon.nicolas@gmail.com>
2023-08-17 16:13:20 +0200

    Codelab Workmanager

---
+++ basic-android-kotlin-compose-training-workmanager/app/src/main/java/com/example/bluromatic/workers/SaveImageToFileWorker.kt
@@ -0,0 +1,70 @@
+package com.example.bluromatic.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+const val SAVE_IMAGE_TAG = "SaveImageToFileWorker"
+
+class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+
+    private val title = "Blurred Image"
+    private val dateFormatter = SimpleDateFormat(
+        "yyyy.MM.dd 'at' HH:mm:ss z",
+        Locale.getDefault()
+    )
+
+    override suspend fun doWork(): Result {
+
+        makeStatusNotification(
+            applicationContext.resources.getString(R.string.saving_image),
+            applicationContext
+        )
+
+        return withContext(Dispatchers.IO) {
+            delay(DELAY_TIME_MILLIS)
+
+            val resolver = applicationContext.contentResolver
+            return@withContext try {
+                val resourceUri = inputData.getString(KEY_IMAGE_URI)
+                val bitmap = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
+                )
+                val imageUrl = MediaStore.Images.Media.insertImage(
+                    resolver,
+                    bitmap,
+                    title,
+                    dateFormatter.format(Date())
+                )
+                if (!imageUrl.isNullOrEmpty()) {
+                    val output = workDataOf(KEY_IMAGE_URI to imageUrl)
+
+                    Result.success()
+                } else {
+                    Log.e(
+                        SAVE_IMAGE_TAG,
+                        applicationContext.resources.getString(R.string.writing_to_mediaStore_failed)
+                    )
+                    Result.failure()
+                }
+            } catch (exceptions: Exception) {
+                Result.failure()
+            }
+        }
+    }
+
+}
